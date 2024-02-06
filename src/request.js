54e5ddbbb62cc8647da472ae2fc7fa7e2680f69e
akhilesh <akhileshk1610@gmail.com>
2024-02-06 14:17:30 +0530

    first commit

--- src/request.js
+++ src/request.js
@@ -1,13 +1,13 @@
-const APIKEY="e9d3243e0715d23c030a359d8341d516";
+import "dotenv/config";
 const requests={
-    fetchTrending:`/trending/all/day?api_key=${APIKEY}&language=en-US`,
-    fetchTopRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`,
-   fetchPopular:   `/movie/popular?api_key=${APIKEY}&language=en-US&page=1`,
-   fetchActionMovies:`/discover/movie?api_key=${APIKEY}&language=en-US&&with_genres=28`,
-   fetchComedyMovies:`/discover/movie?api_key=${APIKEY}&language=en-US&&with_genres=35`,
-fetchHorrorMovies:`/discover/movie?api_key=${APIKEY}&language=en-US&with_genres=27`,
-fetchRomanceMovies:`/discover/movie?api_key=${APIKEY}&&language=en-US&with_genres=10749`,
-fetchDocumentaries:`/discover/movie?api_key=${APIKEY}&language=en-US&&with_genres=99`
+    fetchTrending:`/trending/all/day?api_key=${process.env.tmdbAPIKEY}&language=en-US`,
+    fetchTopRated:`/movie/top_rated?api_key=${process.env.tmdbAPIKEY}&language=en-US&page=1`,
+   fetchPopular:   `/movie/popular?api_key=${process.env.tmdbAPIKEY}&language=en-US&page=1`,
+   fetchActionMovies:`/discover/movie?api_key=${process.env.tmdbAPIKEY}&language=en-US&&with_genres=28`,
+   fetchComedyMovies:`/discover/movie?api_key=${process.env.tmdbAPIKEY}&language=en-US&&with_genres=35`,
+fetchHorrorMovies:`/discover/movie?api_key=${process.env.tmdbAPIKEY}&language=en-US&with_genres=27`,
+fetchRomanceMovies:`/discover/movie?api_key=${process.env.tmdbAPIKEY}&&language=en-US&with_genres=10749`,
+fetchDocumentaries:`/discover/movie?api_key=${process.env.tmdbAPIKEY}&language=en-US&&with_genres=99`
 
 }
 export default requests;
